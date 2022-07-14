3a49d89114ed5ec13935795367a6143ab29abf68
orbayemre <88451419+orbayemre@users.noreply.github.com>
2022-07-14 16:54:19 +0300

    Update homeAPI.js

--- model/homeAPI.js
+++ model/homeAPI.js
@@ -8,7 +8,7 @@ headerWeat = (lat,lon) =>{
     .then(result => {ress(result);})
     .catch(error => {
         alert(error);
-        window.location="file:///E:/Dosyalarim/Web%20Projelerim/Weather-Forecasting/view/index.html";
+        window.location="https://weather-forecastingapp.netlify.app/view/index.html";
     })
 
 
